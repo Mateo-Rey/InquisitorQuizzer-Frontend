373f52d88d5b7e86633b492e7c44be563fbe3175
Mateo-Rey <jrey3darts@gmail.com>
2023-01-11 14:05:52 -0500

    expanded utility

--- src/App.js
+++ src/App.js
@@ -1,43 +1,68 @@
 import "./App.css";
 import "./normalize.css";
 import Avatar from "./assets/chatgptavatar.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
   const [input, setInput] = useState("");
-  const [chatLog, setChatLog] = useState([
-    
-  ]);
+  const [models, setModels] = useState([]);
+  const [temperature, setTemperature] = useState();
+  const [currentModel, setCurrentModel] = useState("ada");
+  const [chatLog, setChatLog] = useState([]);
+
+  const clearChat = () => {
+    setChatLog([]);
+  };
+  useEffect(() => {
+    getEngines();
+  }, []);
+  const getEngines = async () => {
+    fetch("http://localhost:3080/models")
+      .then((response) => response.json())
+      .then((data) => setModels(data.models));
+  };
   console.log(chatLog);
   const handleSubmit = async (e) => {
     e.preventDefault();
     let chatLogNew = [...chatLog, { user: "Me", message: `${input}` }];
     setInput("");
-    setChatLog([...chatLogNew])
-    const messages = chatLogNew.map((message) => message.message).
-    join("\n");
+    setChatLog([...chatLogNew]);
+    const messages = chatLogNew.map((message) => message.message).join("\n");
     const response = await fetch("http://localhost:3080/", {
       method: "POST",
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         message: messages,
-      })
-    })
+        currentModel,
+      }),
+    });
     const data = await response.json();
-    setChatLog([...chatLogNew, { user: "gpt", message: `${data.message}`}]);
+    setChatLog([...chatLogNew, { user: "gpt", message: `${data.message}` }]);
+    console.log(data.message)
   };
   return (
     <div className="App">
       <aside className="sidemenu">
-        <div className="sidemenubutton">
+        <div className="sidemenubutton" onClick={clearChat}>
           <span>+</span>
           New Chat
+        </div>
+        <div className="temperature">
+          <input value={temperature} type="range" min="1" max="10"></input>
+        </div>
+        <div className="models">
+          <select onChange={(e) => setCurrentModel(e.target.value)}>
+            {models.map((model, index) => (
+              <option key={model.id} value={model.id}>
+                {model.id}
+              </option>
+            ))}
+          </select>
         </div>
       </aside>
       <section className="chatbox">
         <div className="chatlog">
           {chatLog.map((message, index) => (
             <ChatMessage key={index} message={message} />
-            
           ))}
         </div>
 
